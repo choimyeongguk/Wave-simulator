85c9411322b0a9f6a1f3f61ec0ec7b1eb3be1bb5
chlar <choimk0512@gmail.com>
2022-05-11 12:00:49 +0900

    사인파 코드 보기좋게 수정

--- interference_sine.py
+++ interference_sine.py
@@ -1,24 +1,20 @@
+from time import sleep
 import matplotlib.pyplot as plt
 import numpy
-from time import sleep
 
 print("파동1")
-waveLength_1 = int(input('파장 : '))
-amplitude_1 = int(input('진폭 : '))
+waveLength_1 = int(input('-파장 : '))
+amplitude_1 = int(input('-진폭 : '))
 print("파동2")
-waveLength_2 = int(input('파장 : '))
-amplitude_2 = int(input('진폭 : '))
+waveLength_2 = int(input('-파장 : '))
+amplitude_2 = int(input('-진폭 : '))
 
 x_1 = [];y_1 = []
 x_2 = [];y_2 = []
 X = [];Y = []
 for i in range(0, 4*waveLength_1):
- x_1.append(i)
- x_2.append(i)
- X.append(i)
- y_1.append(0)
- y_2.append(0)
- Y.append(0)
+ x_1.append(i);x_2.append(i);X.append(i)
+ y_1.append(0);y_2.append(0);Y.append(0)
 
 plt.ion()
 fig = plt.figure(figsize=(8,4))
@@ -31,7 +27,6 @@ ax.plot([-4*waveLength_1*0.1, 4*waveLength_1*1.1],[0,0], color='black', lw=3)
 wave1, = ax.plot(x_1, y_1, color='blue', lw=2)
 wave2, = ax.plot(x_2, y_2, color='blue', lw=2)
 wave3, = ax.plot(X, Y, color='red', lw=3.5)
-
 
 num=0
 while(True):
